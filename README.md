f170f0e3f8a0780d367b413acdf7a8ed9e4ae108
Libish Jacob <jajac@vestas.com>
2018-04-18 16:22:52 +0530

    More examples added
    More examples added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Test applications and projects
 
 For reactive extension to work, we need "System.Reactive" nuget package.
+
+refer http://rxwiki.wikidot.com/101samples for more examples.
--- ReactiveExtensions/ReactiveExtensions/MainWindow.xaml.cs
+++ ReactiveExtensions/ReactiveExtensions/MainWindow.xaml.cs
@@ -12,10 +12,12 @@ namespace ReactiveExtensions
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private event EventHandler<string> Event;
+
         public MainWindow()
         {
-            InitializeComponent();
-            MainMethod();
+            InitializeComponent();            
         }
 
         static IEnumerable<string> EndlessBarrageOfEmail()
@@ -25,28 +27,74 @@ namespace ReactiveExtensions
             for (; ; )
             {
                 // Return some random emails at random intervals.
+                // this will start to send an edless stream of items.
                 yield return emails[random.Next(emails.Count)];
                 Thread.Sleep(random.Next(1000));
             }
         }
-        static void MainMethod()
+        void MainMethod()
         {
             var myInbox = EndlessBarrageOfEmail().ToObservable();
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            ////myInbox.Subscribe(email =>
+            ////{
+            ////    var executionThreadId = Thread.CurrentThread.ManagedThreadId;
+            ////    Console.WriteLine($"You've got {email.Count()} new messages!  Here they are! This is initaited by {threadId} executed on {executionThreadId}");
+                                
+            ////    Console.WriteLine("> {0}", email);                
+            ////    Console.WriteLine();
+            ////});
+            
 
-            // Instead of making you wait 5 minutes, we will just check every three seconds instead. :)
+            // Instead of making you wait 5 minutes, we will just check every three seconds instead.  
+            // here as the result is an endless stream of data, we will process it in batch collected in 3 secs interval
             var getMailEveryThreeSeconds = myInbox.Buffer(TimeSpan.FromSeconds(3)); //  Was .BufferWithTime(...
 
+            // Here the method EndlessBarrageOfEmail will not be executed untill subscribe is called. So EndlessBarrageOfEmail is executed by main thread here. So it will wait for the method to exit.
+            // The callbacks are executed on a seperate thread.
             getMailEveryThreeSeconds.Subscribe(emails =>
-            {
-                Console.WriteLine("You've got {0} new messages!  Here they are!", emails.Count());
+            {   
+                var executionThreadId = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"You've got {emails.Count()} new messages!  Here they are! This is initaited by {threadId} executed on {executionThreadId}");
                 foreach (var email in emails)
-                {
+                {   
                     Console.WriteLine("> {0}", email);
                 }
                 Console.WriteLine();
-            });
-
-            Console.ReadKey();
+            });            
         }
+
+        private void BatchExecution_Click(object sender, RoutedEventArgs e)
+        {
+            MainMethod();
+        }
+
+        private void NetEventAsObservable_Click(object sender, RoutedEventArgs e)
+        {
+            // dotnet event is made as suscribeable.
+
+            var observableItem = Observable.FromEventPattern<string>(ev => Event += ev, ev => Event -= ev);
+            var s = observableItem.Subscribe(args => Console.WriteLine("Received event for s subscriber"));
+            var t = observableItem.Subscribe(args => Console.WriteLine("Received event for t subscriber"));
+
+            Event(null, "hi");
+
+            s.Dispose();
+            t.Dispose();
+        }
+
+        private void IntervalSubscription_Click(object sender, RoutedEventArgs e)
+        {   
+            var clock = Observable.Interval(TimeSpan.FromSeconds(1))
+            .Select((t, index) => DateTimeOffset.Now);
+         
+            clock.Subscribe(timeIndex =>
+            {
+                Console.WriteLine("Ding dong.  The time is now {0}.", timeIndex);
+            });
+        }
+
+
+        /// have a look at Delay, Sample etc....
     }
 }
